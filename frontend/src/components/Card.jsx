24b60b98eb382c3bda553ffed5d80f78da387287
vips11 <vipasha2002@gmail.com>
2023-01-14 18:04:11 -0500

    CENETRA-14: Created the card component

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Card = ({ isLog }) => {
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.headerRow}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          {isLog ? null : <View style={styles.dotContainer} />}
+          <Text style={styles.titleText}>Title</Text>
+        </View>
+        <Text style={styles.timeText}>Time</Text>
+      </View>
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateText}>Created on...</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: 390,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  dotContainer: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "pink",
+    marginRight: 8,
+  },
+  titleText: {
+    fontWeight: "500",
+    fontSize: 16,
+    color: "#363535",
+    textAlign: "center",
+  },
+  timeText: {
+    color: "#A2A2A2",
+    fontSize: 14,
+    fontWeight: "500",
+    textAlign: "center",
+  },
+  dateContainer: {},
+  dateText: {
+    color: "#A2A2A2",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+});
