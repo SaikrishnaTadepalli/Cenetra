147396ef6fa5b40815e0a213860ea8787444a510
vips11 <vipasha2002@gmail.com>
2023-01-15 13:36:05 -0500

    CENETRA-8: Created individual notice card and screen

--- frontend/src/screens/NoticeInfoScreen.jsx
+++ frontend/src/screens/NoticeInfoScreen.jsx
@@ -1,14 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import NoticeCard from "../components/NoticeCard";
 
 const NoticeInfoScreen = () => {
   return (
-    <View>
-      <Text>NoticeInfoScreen</Text>
+    <View style={styles.container}>
+      <NoticeCard />
     </View>
   );
 };
 
 export default NoticeInfoScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "center",
+  },
+});
