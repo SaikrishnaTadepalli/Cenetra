646d9082462b6be6775dc94ada6ffb46a30c9849
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-01-16 03:18:08 -0500

    git ignore credentials

--- backend/utils/s3.js
+++ backend/utils/s3.js
@@ -1,30 +1,25 @@
 const AWS = require("aws-sdk");
-if (process.env.NODE_ENV !== "production") {
-  AWS.config.loadFromPath("./credentials.json");
-}
-// else {
-// make sure to set environment variables in production for:
-// AWS_ACCESS_KEY_ID
-// AWS_SECRET_ACCESS_KEY
-// and aws will automatically use those environment variables
-// }
+
+//   AWS.config.loadFromPath("./credentials.json");
+
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 const singleFileUpload = async (file) => {
   const { filename, mimetype, createReadStream } = await file;
+
   const fileStream = createReadStream();
   const path = require("path");
+
   // name of the file in your S3 bucket will be the date in ms plus the extension name
   const Key = new Date().getTime().toString() + path.extname(filename);
+
   const uploadParams = {
-    // name of your bucket here
     Bucket: "cenetra-media-dev",
     Key,
     Body: fileStream,
   };
-  const result = await s3.upload(uploadParams).promise();
 
-  // save the name of the file in your bucket as the key in your database to retrieve for later
+  const result = await s3.upload(uploadParams).promise();
   return result.Key;
 };
 
